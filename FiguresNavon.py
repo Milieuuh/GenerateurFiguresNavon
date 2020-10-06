5d29a59c22233df265cf528173fd0280d9640192
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-10-06 13:57:04 +0200

    ajout des attributs et méthodes

--- FiguresNavon.py
+++ FiguresNavon.py
@@ -2,7 +2,32 @@
 
 class FigureNavon:
 
+    # ATTRIBUTS
+    elementGlobal = ""
+    elementLocal = ""
+    listeFiguresNavon = []
+
+    #CONSTRUCTEUR
     def __init__(self, elementG, elementL):
         self.elementGlobal = elementG
         self.elementLocal = elementL
         
+
+
+    #METHODES    
+    def ajouterFigureNavon(newFigureNavon):
+        listeFiguresNavon.append(newFigureNavon)
+
+    def sauvegarderFigure():
+        print("sauvegarde")
+
+        
+    ###############################################GETTER
+    def getElementGlobal():
+        return self.elementGlobal
+
+    def getElementLocal():
+        return self.elementLocal
+
+    def getListeFiguresNavon():
+        return self.listeFiguresNavon
