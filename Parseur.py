a155db333eeccf0b1276a86dd100e504535ada70
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-10-06 14:06:43 +0200

    Update Parseur.py

---
+++ Parseur.py
@@ -0,0 +1,25 @@
+
+
+
+class Parseur:
+
+    #ATTRIBUTS
+    elementGlobal = ""
+    nomFichier = ""
+
+
+    #CONSTRUCTEUR
+    def __init__(self, nomFile):
+        self.nomFichier=nomFile
+
+
+    #METHODES
+    def lireFichier():
+        print("Lecture en cours du fichier")
+
+
+    ##############################GETTER
+    def getNomFichier():
+        return self.nomFichier
+
+    
