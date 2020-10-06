cd8761c2972792dab980d43a2cdd394af014377a
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-10-06 18:11:11 +0200

    ajout templates +codage parseur

--- Metier/Parseur.py
+++ Metier/Parseur.py
@@ -1,5 +1,4 @@
-
-
+import json
 
 class Parseur:
 
@@ -9,17 +8,49 @@ class Parseur:
 
 
     #CONSTRUCTEUR
-    def __init__(self, nomFile):
+    def __init__(self, nomFile, elementG):
         self.nomFichier=nomFile
+        self.elementGlobal = elementG
+        self.listeCoordonnees = []
 
 
     #METHODES
-    def lireFichier():
+        
+      #permet de lire le fichier de description du symbole
+    def lireFichier(self):
         print("Lecture en cours du fichier")
 
+        with open("../Templates/"+self.nomFichier, 'r') as fichier:
+            data = json.load(fichier)
 
+        self.recupererPoints(data)
+
+        print(self.listeCoordonnees)
+
+        
+       
+
+        #récupère les points de départ et d'arrivée des différentes droites qui composent l'élément global
+    def recupererPoints(self,data):
+        i=0
+        for x in data:
+             i=i+1
+
+        
+        for nb in range(0,i):
+            self.listeCoordonnees.append(int(data['Droite '+ str(nb)]['Point Depart X']))
+            self.listeCoordonnees.append(int(data['Droite '+ str(nb)]['Point Depart Y']))
+            self.listeCoordonnees.append(int(data['Droite '+ str(nb)]['Point Arrive X']))
+            self.listeCoordonnees.append(int(data['Droite '+ str(nb)]['Point Arrive Y']))
+
+
+
+
+        
     ##############################GETTER
     def getNomFichier():
         return self.nomFichier
 
     
+P = Parseur("lettreA.json","N")
+P.lireFichier()
