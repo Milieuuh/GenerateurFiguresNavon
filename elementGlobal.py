6cc1441b8e90cfa52479104ae43d0533b6653ddc
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-10-06 14:18:11 +0200

    Create elementGlobal.py

---
+++ elementGlobal.py
@@ -0,0 +1,35 @@
+
+
+class ElementGlobal:
+
+
+    #ATTRIBUTS
+    tailleGlobalX = 800
+    tailleGlobalY = 800
+
+
+    #CONSTRUCTEUR
+    def __init__(self, tailleX,tailleY):
+        self.tailleGlobalX = tailleX
+        self.tailleGlobalY = tailleY
+
+
+    ##############GETTER ET SETTER
+    def setTailleGlobalX(newTailleX):
+        self.tailleGlobalX = newTailleX
+
+    def setTailleGlobalY(newTailleY):
+        self.tailleGlobalY = newTailleY
+
+    def getTailleGlobalX():
+         return self.tailleGlobalX
+
+
+    def getTailleGlobalY():
+        return self.tailleGlobalY
+
+    
+
+
+    
+
