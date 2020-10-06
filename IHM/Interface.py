3ac53c3951085d9a5715ae38b66083887eae6ffa
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-10-06 14:42:47 +0200

    Update Interface.py

--- IHM/Interface.py
+++ IHM/Interface.py
@@ -8,3 +8,20 @@ class Interface:
 
     def __init__(self):
         print("constructeur")
+
+    def fenetreApropos():
+        print("a voir pour apropos")
+
+#Création de la fenêtre principale
+        monInterface = Tk()
+
+        monInterface.title("générateur de Navon")
+        monInterface.geometry("300x400")
+
+
+
+#Création d'un bouton aPropos
+        bt_aPropos = Button(monInterface, text="i",command=fenetreApropos)
+        bt_aPropos.grid(row=1,column=0,padx=5,pady=50)
+
+        monInterface.mainloop()
