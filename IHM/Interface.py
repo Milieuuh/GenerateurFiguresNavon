28288b01e5d61dac96b7358af25ea010bb93e7ce
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-10-07 09:27:29 +0200

    k

--- IHM/Interface.py
+++ IHM/Interface.py
@@ -3,25 +3,77 @@ import tkinter
 import aider
 import aPropos
 
-class Interface:
 
 
-    def __init__(self):
-        print("constructeur")
+#generer les fichiers png
+def generer():
+    res_LettreGlobale=lettreGlobale.get()
+    res_LettreLocale=lettreLocale.get()
+    print("generer à faire : ")
+        
 
-    def fenetreApropos():
-        print("a voir pour apropos")
+#fenetrePrincipale
+maFenetre = tkinter.Tk()
 
-#Création de la fenêtre principale
-        monInterface = Tk()
+maFenetre.title("générateur de Navon")
+maFenetre.geometry("700x800")
 
-        monInterface.title("générateur de Navon")
-        monInterface.geometry("300x400")
+Titre = tkinter.StringVar()
+l_lettreGlobale = tkinter.Label(maFenetre, textvariable=Titre)
+Titre.set("------ Générateur de Navon  ------")
+l_lettreGlobale.grid(row=0,column=10,padx=0,pady=0)
+
+
+#--------------------------------------------------------------------- FORME DES LETTRES
+   
+#Label lettre Globale et champs de saisie
+Text = tkinter.StringVar()
+l_lettreGlobale = tkinter.Label(maFenetre, textvariable=Text)
+Text.set("Lettre Globale : ")
+l_lettreGlobale.grid(row=1,column=1,padx=5,pady=100)
+
+lettreGlobale=tkinter.StringVar()
+lettreGlobale.set("A")
+champ_lettreGlobale = tkinter.Entry(maFenetre,textvariable=lettreGlobale, bg="white", width="10")
+champ_lettreGlobale.grid(row=1,column=10,padx=5,pady=235)
+
+
+#Label lettre locale et champ de saisie
+Text2 = tkinter.StringVar()
+l_lettreLocale = tkinter.Label(maFenetre, textvariable=Text2)
+Text2.set("Lettre Locale : ")
+l_lettreLocale.grid(row=1,column=15,padx=5,pady=100)
+
+lettreLocale=tkinter.IntVar()
+lettreLocale.set("B")
+champ_lettreLocale =  tkinter.Entry(maFenetre,textvariable=lettreLocale, bg="white", width="10")
+champ_lettreLocale.grid(row=1,column=20,padx=5,pady=235)
+
+
+#---------------------------------------------------------------------TAILLE DES LETTRES GLOBALES
+t_tailleGX = tkinter.StringVar()
+l_tailleGX = tkinter.Label(maFenetre, textvariable=t_tailleGX)
+t_tailleGX.set("Lettre Locale : ")
+l_tailleGX.grid(row=2,column=1,padx=10,pady=100)
+
+nb_tailleGX = tkinter.IntVar()
+nb_tailleGX.set(800)
+champ_tailleGX = tkinter.Entry(maFenetre,textvariable=nb_tailleGX, bg="white", width="10")
+champ_tailleGX.grid(row=2,column=10,padx=0,pady=235)
 
 
 
-#Création d'un bouton aPropos
-        bt_aPropos = Button(monInterface, text="i",command=fenetreApropos)
-        bt_aPropos.grid(row=1,column=0,padx=5,pady=50)
 
-        monInterface.mainloop()
+
+#--------------------------------------------------------------------- BOUTON GENERER
+
+#Bouton générer
+bt_Generer = tkinter.Button(maFenetre,text="Générer",command=generer)
+bt_Generer.grid(row=5,column=10,padx=15,pady=5)
+
+#--------------------------------------------------------------------- GENERATION FENETRE
+
+#CREATION FENETRE
+maFenetre.mainloop()
+
+
--- IHM/aPropos.py
+++ IHM/aPropos.py
@@ -1,4 +1,8 @@
+import tkinter
+
 class aPropos:
 
     def __init__(self):
         print("aPropos")
+
+  
