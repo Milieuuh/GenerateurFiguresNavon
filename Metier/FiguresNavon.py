2fb54e8c0c6ccf5cfb734ea077fc529971906a87
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-11-06 15:27:16 +0100

    essai

--- Metier/FiguresNavon.py
+++ Metier/FiguresNavon.py
@@ -1,5 +1,6 @@
 import elementGlobal, elementLocal, Parseur
 from PIL import Image, ImageDraw
+from math import *
 
 class FigureNavon:
 
@@ -23,7 +24,7 @@ class FigureNavon:
         self.nbCaracteresLocaux = nbCaracteresLocaux
         self.valeurMoyenneDeSymboles = 0
         self.nombreDeSegmentsDansLettre = 0
-        self.listeTailleDesSegment = []
+        self.listeTailleDesSegments = []
         self.listeFiguresNavon = []
 
 
@@ -32,7 +33,8 @@ class FigureNavon:
     def calculMesureTailleSegments(self, Xa, Ya, Xb, Yb):
         print("calcul taille de la lettre")
         self.mesureTailleSegments = self.mesureTailleSegments +((Ya-Xa)**2 +(Yb-Xb)**2)**(1/2)
-        self.listeTailleDesSegment.append(((Ya-Xa)**2 +(Yb-Xb)**2)**(1/2))
+        print(((Ya-Xa)**2 +(Yb-Xb)**2)**(1/2))
+        self.listeTailleDesSegments.append(((Ya-Xa)**2 +(Yb-Xb)**2)**(1/2))
         
 
 
@@ -52,13 +54,15 @@ class FigureNavon:
             self.calculMesureTailleSegments(self.parser.get(i)*self.tailleLG//100, self.parser.get(i+1)*self.tailleLG//100, self.parser.get(i+2)*self.tailleLG//100, self.parser.get(i+3)*self.tailleLG//100)
             self.nombreDeSegmentsDansLettre=self.nombreDeSegmentsDansLettre+1
             print(self.mesureTailleSegments)
-            i=i+4
+            i = i+4
 
         i=0
-        
+        compteur =0
         while i<len(self.parser.getListeCoordonnees()):
-             self.placementElementsLocaux(self.parser.get(i)*self.tailleLG//100, self.parser.get(i+1)*self.tailleLG//100, self.parser.get(i+2)*self.tailleLG//100, self.parser.get(i+3)*self.tailleLG//100, img1)
-             i=i+4
+             self.placementElementsLocaux(self.parser.get(i)*self.tailleLG//100, self.parser.get(i+1)*self.tailleLG//100, self.parser.get(i+2)*self.tailleLG//100, self.parser.get(i+3)*self.tailleLG//100, img1, compteur)
+             compteur = compteur + 1
+             i = i+4
+
             
        
         img_figure_navon.show()
@@ -67,33 +71,35 @@ class FigureNavon:
         #self.sauvegarderFigure(img_figure_navon)
 
  
-    def calculEquationDroite(self, Xa, Ya, Xb, Yb, img):
+    def calculEquationDroite(self, Xa, Ya, Xb, Yb, img, numSegment):
         m= (Yb-Ya)/(Xb-Xa)
         p= Ya - Xa*m
         i=Xa
-        ecart = self.mesureTailleSegments/self.nbCaracteresLocaux
-        while i<Xb:
+        nb = ceil(self.listeTailleDesSegments[numSegment])*self.nbCaracteresLocaux/self.mesureTailleSegments
+        ecart = ceil(self.listeTailleDesSegments[numSegment])/nb
+        while i<Xa+ceil(self.listeTailleDesSegments[numSegment]):
             y= m*i+p
-            texte = img.multiline_text((i,y), str(self.elementLocal), fill=(0, 0, 0))
-            i=i+ecart
+            img.multiline_text((i, y), str(self.elementLocal), fill=(0, 0, 0))
+            i = i+int(ecart)
         
 
-    def placementElementsLocaux(self, Xa, Ya, Xb, Yb, img):        
+    def placementElementsLocaux(self, Xa, Ya, Xb, Yb, img, numSegment):
         #calcul de l'équation des droites
             #coeff directeur m et de p
-        if Xb-Xa!=0:
-            if Xb>Xa:
-               self.calculEquationDroite(Xa, Ya, Xb, Yb, img)
+        if Xb-Xa != 0:
+            if Xb > Xa:
+               self.calculEquationDroite(Xa, Ya, Xb, Yb, img, numSegment)
             else:
-                self.calculEquationDroite(Xb, Yb, Xa, Ya, img)
+                self.calculEquationDroite(Xb, Yb, Xa, Ya, img, numSegment)
 
             #x=k, k étant une constante
-        elif Xa==Xb:
+        elif Xa == Yb:
             y=Ya
-            ecart = self.mesureTailleSegments / self.nbCaracteresLocaux
-            while y<Yb:
-                texte = img.multiline_text((Xa,y), str(self.elementLocal), fill=(0, 0, 0))
-                y= y+ecart
+            nb = ceil(self.listeTailleDesSegments[numSegment]) * self.nbCaracteresLocaux / self.mesureTailleSegments
+            ecart = ceil(self.listeTailleDesSegments[numSegment]) / ceil(nb)
+            while y < Ya+ceil(self.listeTailleDesSegments[numSegment]):
+                img.multiline_text((Xa, y), str(self.elementLocal), fill=(0, 0, 0))
+                y= y+int(ecart)
 
     def ajouterFigureNavon(self, newFigureNavon):
         self.listeFiguresNavon.append(newFigureNavon)
@@ -137,6 +143,3 @@ class FigureNavon:
     def setNbCaractereLocaux(self,nb):
         self.nbCaracteresLocaux=nb
 
-
-#F = FigureNavon("L","N") 
-#F.creerFigureNavon()
