9c153480cd986a93f7327999131a1d74a36a5bd9
BrightHammer21 <lucieboucquemont@hotmail.fr>
2021-02-01 12:01:27 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Milieuuh/GenerateurFiguresNavon""
    This reverts commit ef996f67da263a346f0a4ecf783d1eb86ab8d033.

--- Metier/FiguresNavon.py
+++ Metier/FiguresNavon.py
@@ -202,25 +202,32 @@ class FigureNavon:
                 y = y0 + sqrt(r**2 - (x-x0)**2)
                 imgDraw.text((x, y), str(self.elementLocal), fill=(0, 0, 0), font=font)
                 x = r * math.cos(math.pi * compteur/ (nbElementSurMonSegment/2))+x0
-                compteur = compteur + 1
+                compteur = compteur +1
 
-            #demi cercle à droite
-            elif angleDepart > angleArrive:
+            #demi cercle à gauche
+            elif angleArrive >angleDepart:
                 y = y0 + sqrt(r ** 2 - (x - x0) ** 2)
                 y2 = y0 - sqrt(r ** 2 - (x - x0) ** 2)
                 imgDraw.text((x, y), str(self.elementLocal), fill=(0, 0, 0), font=font)
                 imgDraw.text((x, y2), str(self.elementLocal), fill=(0, 0, 0), font=font)
-                x = r * math.cos(math.pi * 1/2-2 * compteur / (nbElementSurMonSegment / 2)) + x0
+                x = r * math.cos(math.pi * -1/2-2 * compteur / (nbElementSurMonSegment / 2)) + x0
                 compteur = compteur + 1
 
-            #demi cercle à gauche
+            elif self.elementGlobal == "G" or self.elementGlobal == "g":
+                y = y0 + sqrt(r ** 2 - (x - x0) ** 2)
+                imgDraw.text((x, y), str(self.elementLocal), fill=(0, 0, 0), font=font)
+                x = r * math.cos(math.pi * 1 / 4 - 2 * compteur / (nbElementSurMonSegment / 2)) + x0
+                compteur = compteur + 1
+
+            #demi cercle à droite
             else:
                 y = y0 + sqrt(r ** 2 - (x - x0) ** 2)
                 y2 = y0 - sqrt(r ** 2 - (x - x0) ** 2)
                 imgDraw.text((x, y), str(self.elementLocal), fill=(0, 0, 0), font=font)
                 imgDraw.text((x, y2), str(self.elementLocal), fill=(0, 0, 0), font=font)
-                x = r * math.cos(math.pi * -1 /2 - 2 * compteur / (nbElementSurMonSegment / 2)) + x0
+                x = r * math.cos(math.pi * 1 /2 - 2 * compteur / (nbElementSurMonSegment / 2)) + x0
                 compteur = compteur + 1
+
 
 
 
